c26274d9deaf04938ba4abf73cadcc34a359d2bc
Joe Mou <joe@mou.fo>
2019-01-26 03:43:09 -0500

    WIP messy xml-rs

--- src/main.rs
+++ src/main.rs
@@ -6,67 +6,50 @@
 // August is a plaintext alternative to html2md. https://gitlab.com/alantrick/august/
 
 use chrono::{DateTime, Local};
+use failure::{Error, Fail};
 use html2md::parse_html;
 use pulldown_cmark::{html, Parser};
-use roxmltree::{Document, NodeType};
 use std::ffi::OsStr;
 use std::fmt;
-use std::fs;
-use std::io::{stdout, Write};
+use std::fs::File;
+use std::io::{stdout, BufReader, Read, Write};
 use std::path::Path;
 use std::str;
+use xml::reader::{EventReader, ParserConfig, XmlEvent};
 
-#[derive(Debug)]
-enum Error {
-    Io(std::io::Error),
-    Xml(roxmltree::Error),
-    Chrono(chrono::format::ParseError),
+#[derive(Debug, Fail)]
+enum AppError {
+    #[fail(display = "I/O error")]
+    Io(#[cause] std::io::Error),
+    #[fail(display = "XML error")]
+    Xml(#[cause] xml::reader::Error),
+    #[fail(display = "Chrono error")]
+    Chrono(#[cause] chrono::format::ParseError),
+    #[fail(display = "Parse error: {}", _0)]
     Parse(String),
 }
 
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Error {
-        Error::Io(e)
+impl From<std::io::Error> for AppError {
+    fn from(e: std::io::Error) -> AppError {
+        AppError::Io(e)
     }
 }
 
-impl From<roxmltree::Error> for Error {
-    fn from(e: roxmltree::Error) -> Error {
-        Error::Xml(e)
+impl From<xml::reader::Error> for AppError {
+    fn from(e: xml::reader::Error) -> AppError {
+        AppError::Xml(e)
     }
 }
 
-impl From<chrono::format::ParseError> for Error {
-    fn from(e: chrono::format::ParseError) -> Error {
-        Error::Chrono(e)
+impl From<chrono::format::ParseError> for AppError {
+    fn from(e: chrono::format::ParseError) -> AppError {
+        AppError::Chrono(e)
     }
 }
 
-type Result<T> = std::result::Result<T, Error>;
+type Result<T> = std::result::Result<T, AppError>;
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::Io(e) => e.fmt(f),
-            Error::Xml(e) => e.fmt(f),
-            Error::Chrono(e) => e.fmt(f),
-            Error::Parse(s) => s.fmt(f),
-        }
-    }
-}
-
-impl std::error::Error for Error {
-    fn description(&self) -> &str {
-        match self {
-            Error::Io(e) => e.description(),
-            Error::Xml(e) => e.description(),
-            Error::Chrono(e) => e.description(),
-            Error::Parse(s) => s,
-        }
-    }
-}
-
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 struct NoteAttributes {
     author: Option<String>,
     source_url: Option<String>,
@@ -76,7 +59,7 @@ struct NoteAttributes {
     altitude: Option<String>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 struct Note {
     title: Option<String>,
     content: Option<String>,
@@ -86,57 +69,171 @@ struct Note {
     attributes: NoteAttributes,
 }
 
-// TODO try pull parser again. Use state machine to break up match rules. quick-xml or xml-rs.
-// https://github.com/media-io/yaserde/blob/master/yaserde/src/de/mod.rs
-fn xml_to_notes(buf: &str) -> Result<Vec<Note>> {
-    let doc = Document::parse(buf)?;
-    let node = doc.root_element();
-    if node.node_type() != NodeType::Element || node.tag_name().name() != "en-export" {
-        return Err(Error::Parse("expected <en-export>".to_string()));
+enum EnexParserState {
+    Initial,
+    EnExport,
+    Note(Note),
+    NoteAttributes(Note),
+    Done,
+}
+
+// XXX still necessary?
+struct WrappedReader<R: Read> {
+    reader: EventReader<R>,
+}
+
+fn event_to_error(prefix: &str, event: std::result::Result<XmlEvent, xml::reader::Error>) -> AppError {
+    match event {
+        Ok(e) => AppError::Parse(format!("{} {:?}", prefix, e)),
+        Err(e) => e.into(),
     }
-    let mut notes = Vec::new();
-    for node in node.children() {
-        if node.node_type() == NodeType::Text && node.text().unwrap().trim().is_empty() {
-            continue;
-        }
-        if node.node_type() != NodeType::Element || node.tag_name().name() != "note" {
-            return Err(Error::Parse("expected <note>".to_string()));
-        }
-        let mut note: Note = Default::default();
-        for node in node.children() {
-            if node.node_type() == NodeType::Element {
-                match node.tag_name().name() {
-                    "title" => note.title = node.text().map(str::to_owned),
-                    "content" => note.content = node.text().map(str::to_owned),
-                    "created" => note.created = node.text().map(str::to_owned),
-                    "updated" => note.updated = node.text().map(str::to_owned),
-                    "tag" => note.tags.extend(node.text().map(str::to_owned)),
-                    "note-attributes" => {
-                        let attrs = &mut note.attributes;
-                        for node in node.children() {
-                            if node.node_type() == NodeType::Element {
-                                match node.tag_name().name() {
-                                    "author" => attrs.author = node.text().map(str::to_owned),
-                                    "source" => attrs.source = node.text().map(str::to_owned),
-                                    "source-url" => attrs.source_url = node.text().map(str::to_owned),
-                                    "latitude" => attrs.latitude = node.text().map(str::to_owned),
-                                    "longitude" => attrs.longitude = node.text().map(str::to_owned),
-                                    "altitude" => attrs.altitude = node.text().map(str::to_owned),
-                                    t => return Err(Error::Parse(format!("unexpected <{}>", t))),
-                                }
-                            }
-                        }
+}
+
+impl<R: Read> WrappedReader<R> {
+    fn read_event(&mut self) -> std::result::Result<XmlEvent, xml::reader::Error> {
+        self.reader.next()
+    }
+
+    fn text_and_end(&mut self) -> Result<Option<String>> {
+        match self.read_event() {
+            Ok(XmlEvent::Characters(text)) => {
+                match self.read_event() {
+                    // XXX confirm matching close
+                    Ok(XmlEvent::EndElement { .. }) => { }
+                    x => {
+                        return Err(event_to_error("expected text, found", x));
                     }
-                    "resource" => {
-                        // TODO emit some sort of placeholder
-                    }
-                    t => return Err(Error::Parse(format!("unexpected <{}>", t))),
                 }
+                Ok(Some(text))
+            }
+            // XXX confirm matching close
+            Ok(XmlEvent::EndElement { .. }) => Ok(None),
+            x => {
+                return Err(event_to_error("expected text, found", x));
             }
         }
-        notes.push(note);
     }
-    Ok(notes)
+
+}
+
+struct EnexParser<R: Read> {
+    reader: WrappedReader<R>,
+    state: EnexParserState,
+}
+
+// XXX better handling of tail recursion? may be simple wrap with loop
+// https://users.rust-lang.org/t/when-will-rust-have-tco-tce/20790/2
+impl<R: Read> EnexParser<R> {
+    fn next_helper(&mut self) -> Result<Option<Note>> {
+        match self.state {
+            EnexParserState::Initial => {
+                match self.reader.read_event() {
+                    Ok(XmlEvent::StartDocument { .. }) => { }
+                    x => {
+                        return Err(event_to_error("expected document start, found", x));
+                    }
+                }
+                match self.reader.read_event() {
+                    Ok(XmlEvent::StartElement { ref name, .. }) if name.local_name == "en-export" => { }
+                    x => {
+                        return Err(event_to_error("expected <en-export>, found", x));
+                    }
+                }
+                self.state = EnexParserState::EnExport;
+                self.next_helper()
+            }
+            EnexParserState::EnExport => {
+                match self.reader.read_event() {
+                    Ok(XmlEvent::StartElement { ref name, .. }) if name.local_name == "note" => {
+                        self.state = EnexParserState::Note(Default::default());
+                        self.next_helper()
+                    }
+                    Ok(XmlEvent::EndElement { ref name, .. }) if name.local_name == "en-export" => {
+                        self.state = EnexParserState::Done;
+                        Ok(None)
+                    }
+                    x => {
+                        return Err(event_to_error("expected <note>, found", x));
+                    }
+                }
+            }
+            EnexParserState::Note(ref mut note) => {
+                match self.reader.read_event() {
+                    Ok(XmlEvent::StartElement { ref name, .. }) => {
+                        match name.local_name.as_str() {
+                            "title" => note.title = self.reader.text_and_end()?,
+                            "content" => note.content = self.reader.text_and_end()?,
+                            "created" => note.created = self.reader.text_and_end()?,
+                            "updated" => note.updated = self.reader.text_and_end()?,
+                            "tag" => note.tags.extend(self.reader.text_and_end()?,),
+                            "note-attributes" => {
+                                // XXX this feels really sketchy
+                                self.state = EnexParserState::NoteAttributes(note.clone());
+                            }
+                            "resource" => {
+                                // TODO emit some sort of placeholder
+                                loop {
+                                    match self.reader.read_event() {
+                                        Ok(XmlEvent::EndElement { ref name }) if name.local_name == "resource" => break,
+                                        _ => { }
+                                    }
+                                }
+                            }
+                            t => return Err(AppError::Parse(format!("unexpected <{}>", t))),
+                        }
+                        self.next_helper()
+                    }
+                    // XXX confirm matching end
+                    Ok(XmlEvent::EndElement { .. }) => {
+                        let note = note.clone();
+                        self.state = EnexParserState::EnExport;
+                        Ok(Some(note))
+                    }
+                    x => {
+                        return Err(event_to_error("unexpected in <note>:", x));
+                    }
+                }
+            }
+            EnexParserState::NoteAttributes(ref mut note) => {
+                match self.reader.read_event() {
+                    Ok(XmlEvent::StartElement { ref name, .. }) => {
+                        let attrs = &mut note.attributes;
+                        match name.local_name.as_str() {
+                            "author" => attrs.author = self.reader.text_and_end()?,
+                            "source" => attrs.source = self.reader.text_and_end()?,
+                            "source-url" => attrs.source_url = self.reader.text_and_end()?,
+                            "latitude" => attrs.latitude = self.reader.text_and_end()?,
+                            "longitude" => attrs.longitude = self.reader.text_and_end()?,
+                            "altitude" => attrs.altitude = self.reader.text_and_end()?,
+                            t => return Err(AppError::Parse(format!("unexpected <{}>", t))),
+                        }
+                        self.next_helper()
+                    }
+                    // XXX confirm matching end
+                    Ok(XmlEvent::EndElement { .. }) => {
+                        self.state = EnexParserState::Note(note.clone());
+                        self.next_helper()
+                    }
+                    x => {
+                        return Err(event_to_error("unexpected in <note-attributes>:", x));
+                    }
+                }
+            }
+            EnexParserState::Done => Ok(None)
+        }
+    }
+}
+
+impl<R: Read> Iterator for EnexParser<R> {
+    type Item = Result<Note>;
+
+    fn next(&mut self) -> Option<Result<Note>> {
+        match self.next_helper() {
+            Ok(Some(n)) => Some(Ok(n)),
+            Ok(None) => None,
+            Err(e) => Some(Err(e)),
+        }
+    }
 }
 
 fn enex_to_mf_date(date: &str) -> Result<impl fmt::Display> {
@@ -171,7 +268,7 @@ fn write_as_mf<W: Write>(writer: &mut W, note: &Note) -> Result<()> {
 }
 
 // TODO this is only for development
-fn write_sxs<W: Write>(writer: &mut W, notes: Vec<Note>) -> std::result::Result<(), Box<std::error::Error>> {
+fn write_sxs<W: Write>(writer: &mut W, notes: Vec<Note>) -> std::result::Result<(), Error> {
     writeln!(writer, "<meta charset=utf-8><style>.html, .md {{ display: inline-block; width: 49%; margin: 0; vertical-align: top; overflow-x: hidden }} x.md {{ font-size: 130% }}</style>")?;
     writeln!(writer, "<br>")?;
     for note in notes {
@@ -196,15 +293,23 @@ fn write_sxs<W: Write>(writer: &mut W, notes: Vec<Note>) -> std::result::Result<
     Ok(())
 }
 
-fn main() -> std::result::Result<(), Box<std::error::Error>> {
+fn main() -> std::result::Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
     let input_path = match &args[..] {
         [_, input_path] =>  input_path,
         _ => panic!("Usage: enex2mf input.enex"),
     };
 
-    let buf = fs::read_to_string(input_path)?;
-    let notes = xml_to_notes(&buf)?;
+    let file = File::open(input_path)?;
+    let file = BufReader::new(file);
+
+    // XXX factor ::new()
+    let mut parser = EnexParser {
+        reader: WrappedReader {
+            reader: ParserConfig::new().trim_whitespace(true).cdata_to_characters(true).create_reader(file),
+        },
+        state: EnexParserState::Initial,
+    };
 
     let writer = &mut stdout();
     let notebook_name = Path::new(input_path).file_stem().map(OsStr::to_string_lossy);
@@ -212,8 +317,8 @@ fn main() -> std::result::Result<(), Box<std::error::Error>> {
     let notebook_name = notebook_name.unwrap_or("unknown".into());
     writeln!(writer, "# {} <!-- Metadata: type: Outline; created: 2018-12-19 11:13:04; reads: 9; read: 2018-12-19 17:39:29; revision: 9; modified: 2018-12-19 17:39:29; importance: 0/5; urgency: 0/5; -->", notebook_name)?;
     // TODO dev only. write_sxs(writer, notes)?;
-    for note in notes {
-        write_as_mf(writer, &note)?;
+    for note in parser {
+        write_as_mf(writer, &note?)?;
     }
 
     Ok(())
